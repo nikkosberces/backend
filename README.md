15cec0d301d94d84bfc6a1a23e03ec3a289cb838
nikkosberces <nikkosberces@gmail.com>
2023-05-30 20:52:39 +0800

    adding data in .json

---
+++ README.md
@@ -0,0 +1,26 @@
+# Rest
+
+- RESTful HTTP API
+- Representational State Transfer, aka REST
+- Architectural style for building scalable web apps
+
+URL verb functionality
+"/notes" GET >fetches all resources
+"/notes/10 GET >fetches singe resources
+"/notes" POST >create a new resource
+"/notes/10" DELETE >feclets single resources
+"/notes/10" PUT >updates singe resources  
+"/notes/10" PATCH >replace singe resources
+
+## Status codes
+
+200 Ok
+201 Create
+204 No content
+
+400 Bad Request
+401 Unauthorized
+404 Not found
+
+500 Internal Server Error
+504 Gateway timeout
--- index.js
+++ index.js
@@ -32,6 +32,8 @@ const express = require("express");
 const app = express();
 const PORT = 3001;
 
+app.use(express.json());
+
 let notes = [
   {
     id: 1,
@@ -45,17 +47,57 @@ let notes = [
   },
   {
     id: 3,
-    content: "Get and Post are the most important methods of HTTP portocol",
+    content: "Get and Post are the most important methods of HTTP protocol",
     important: true,
   },
 ];
 
+function generateId() {
+  const maxId =
+    notes.length > 0 ? Math.max(...notes.map((note) => note.id)) : 0;
+  return maxId + 1;
+}
+
 app.get("/", (request, response) => {
-  response.send("<h1>Hello, Express</h1>");
+  response.send("<h1>Hello, Nodemon!</h1>");
 });
 
 app.get("/api/notes", (request, response) => {
-  response.json(notes);
+  response.status(200).json(notes);
+});
+
+app.get("/api/notes/:id", (request, response) => {
+  const id = parseInt(request.params.id);
+  const note = notes.find((note) => note.id === id);
+  console.log(note);
+  response.status(200).json(note);
+});
+
+app.delete("/api/notes/:id", (request, response) => {
+  const id = parseInt(request.params.id);
+  notes = notes.filter((note) => note.id !== id);
+
+  response.status(204).end();
+});
+
+app.post("/api/notes", (request, response) => {
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: "contennt missing",
+    });
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    id: generateId(),
+  };
+
+  notes = notes.concat(note);
+
+  response.status(201).json(note);
 });
 
 app.listen(PORT, () => {
