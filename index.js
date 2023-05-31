ab2e12c356b30cd3900c0cb96a8fd6b0c1ef152c
nikkosberces <nikkosberces@gmail.com>
2023-05-31 20:59:55 +0800

    add put method and connect to the frontend

--- index.js
+++ index.js
@@ -29,9 +29,11 @@
 // console.log(`Server is now running on port ${PORT}`);
 
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const PORT = 3001;
 
+app.use(cors());
 app.use(express.json());
 
 let notes = [
@@ -52,9 +54,31 @@ let notes = [
   },
 ];
 
-function generateId() {
-  const maxId =
-    notes.length > 0 ? Math.max(...notes.map((note) => note.id)) : 0;
+// let persons = [
+//   {
+//     id: 1,
+//     name: "Arto Hellas",
+//     number: "040-123456",
+//   },
+//   {
+//     id: 2,
+//     name: "Ada Lovelace",
+//     number: "39-44-5323523",
+//   },
+//   {
+//     id: 3,
+//     name: "Dan Abramov",
+//     number: "12-43-234245",
+//   },
+//   {
+//     id: 4,
+//     name: "Mary Poppendieck",
+//     number: "39-23-6423122",
+//   },
+// ];
+
+function generateId(db) {
+  const maxId = db.length > 0 ? Math.max(...db.map((note) => note.id)) : 0;
   return maxId + 1;
 }
 
@@ -92,13 +116,78 @@ app.post("/api/notes", (request, response) => {
   const note = {
     content: body.content,
     important: body.important || false,
-    id: generateId(),
+    id: generateId(notes),
   };
 
   notes = notes.concat(note);
 
   response.status(201).json(note);
 });
+
+app.put("/api/notes/:id", (request, response) => {
+  const id = parseInt(request.params.id);
+  const note = notes.find((note) => note.id === id);
+  const { content, important } = request.body;
+
+  const updatedNote = {
+    ...note,
+    content,
+    important,
+  };
+
+  notes = notes.map((note) => (note.id === id ? updatedNote : note));
+  response.status(200).json(updatedNote);
+});
+
+// app.get("/api/persons", (request, response) => {
+//   response.status(200).json(persons);
+// });
+
+// app.get("/info", (request, response) => {
+//   response.send(`<p>Phonebook has info for ${persons.length} people</p>`);
+// });
+
+// app.get("/api/persons/:id", (request, response) => {
+//   const id = parseInt(request.params.id);
+//   const person = persons.find((person) => person.id === id);
+//   console.log(person);
+//   response.status(200).json(person);
+// });
+
+// app.delete("/api/persons/:id", (request, response) => {
+//   const id = parseInt(request.params.id);
+//   persons = persons.filter((person) => person.id !== id);
+
+//   response.status(204).end();
+// });
+
+// app.post("/api/persons", (request, response) => {
+//   const { name, number } = request.body;
+
+//   if (!name || !number) {
+//     return response.status(400).json({
+//       error: "name or number is missing",
+//     });
+//   }
+
+//   const nameExists = persons.some((person) => person.name === name);
+
+//   if (nameExists) {
+//     return response.status(400).json({
+//       error: "name must be unique",
+//     });
+//   }
+
+//   const person = {
+//     name,
+//     number,
+//     id: generateId(persons),
+//   };
+
+//   persons = persons.concat(person);
+
+//   response.status(201).json(person);
+// });
 
 app.listen(PORT, () => {
   console.log(`Server is now running on port ${PORT}`);
